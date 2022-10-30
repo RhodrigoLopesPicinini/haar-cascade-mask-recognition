c53d5730608cb2401490ad633fcdcd18cf686b59
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-10-30 10:35:55 -0300

    Legenda para objetos detectados

--- recognition.py
+++ recognition.py
@@ -9,8 +9,12 @@ objetos = car_cascade.detectMultiScale(gray, 1.01, 5)
 
 print(objetos)
 
+n = 1
 for (x,y,w,h) in objetos:
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.putText(img, str(f"Mask #{n}"), (x, y-5), cv2.FONT_HERSHEY_DUPLEX, 0.4, (0, 0, 255), 1)
+    n += 1
+
 cv2.imshow('Mask Recognition', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
